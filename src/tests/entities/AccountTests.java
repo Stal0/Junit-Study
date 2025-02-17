c77e6d76d081a9fc75951a32512824bec855962a
Lucas Gabriel <lucasgabrielsantos398@gmail.com>
2025-02-17 15:17:01 -0300

    implementation first test with junit

---
+++ src/tests/entities/AccountTests.java
@@ -0,0 +1,29 @@
+package tests.entities;
+
+import entities.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+
+    //Nomenclatura do teste: <AÇÃO> should <EFEITO> [when <CENÁRIO>]
+    //Caso abaixo, Deposito SHOULD (deveria) aumentar o valor da carteira WHEN (Quando) o valor do montante é positivo
+    @Test
+    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+
+        //Padrão AAA
+        //Arrange: Instancie os objetos necessários
+
+        double amount = 200.0;
+        double expectedValue = 196.0;
+
+        Account acc = new Account(1L, 0.0);
+
+        //ACT: Executar as ações necessárias
+        acc.deposit(amount);
+
+        //Assert: Declare o qeu deveria acontecer (Resultado esperado)
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+
+    }
+}
